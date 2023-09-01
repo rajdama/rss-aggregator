978c985947a7cc784ff21a5e91a1270b6c357978
raj <rajdama1729@gmail.com>
2023-09-01 13:03:38 +0530

    server initialization

--- main.go
+++ main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	godotenv.Load(".env")
+	godotenv.Load()
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT not found in env")
 	}
 
-	fmt.Println("PORT: ", portString)
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server listening on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
