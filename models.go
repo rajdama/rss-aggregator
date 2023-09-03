89f349efce684406e65ffc1f964084bbc9cbe365
raj <rajdama1729@gmail.com>
2023-09-03 22:51:42 +0530

    databaseUser To User

---
+++ models.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/rajdama/rss-aggregator/internal/database"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Name      string    `json:"name"`
+}
+
+func databaseUserToUser(dbUser database.User) User {
+	return User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Name:      dbUser.Name,
+	}
+}
